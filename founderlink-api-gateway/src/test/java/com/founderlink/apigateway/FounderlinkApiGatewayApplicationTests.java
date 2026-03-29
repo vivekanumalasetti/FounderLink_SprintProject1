adad27cc3c5dbf22058c9184811bb7f8c90b06d3
Anumalasetti D N S Veerabhadra rao <114346418+vivekanumalasetti@users.noreply.github.com>
2026-03-30 03:39:04 +0530

    Add files via upload

---
+++ founderlink-api-gateway/src/test/java/com/founderlink/apigateway/FounderlinkApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.founderlink.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FounderlinkApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
